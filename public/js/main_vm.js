edaf08952fd31f8e04bed06a6451ec1859dd1fda
Sarah Conway <sarahconway@Sarahs-MacBook-Pro.local>
2020-01-29 08:42:11 -0500

    connected established

--- public/js/main_vm.js
+++ public/js/main_vm.js
@@ -1,1 +1,17 @@
 // imports always go first - if we're importing anything
+
+const socket = io();
+
+// the packet is whatever data we send through with the connect event 
+// from the server 
+function setUserId(packet){
+    console.log(packet);
+}
+
+function showDisconnectMessage() {
+    console.log('a user disconnected');
+}
+
+
+socket.addEventListener('connected', setUserId);
+socket.addEventListener('disconnect', showDisconnectMessage);
